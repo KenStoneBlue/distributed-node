9ee7e371d7d372dacf0c1a6c842a0e5ed37897a9
Ken Stone Blue <ken@stoneblue.com>
2021-03-07 02:10:33 +0000

    project files

--- event-loop-phases.js
+++ event-loop-phases.js
@@ -10,3 +10,35 @@ fs.readFile(__filename, () => {
   process.nextTick(() => console.log(7));
 });
 console.log(8);
+
+const sleep_st = (t) => new Promise((r) => setTimeout(r, t));
+const sleep_im = () => new Promise((r) => setImmediate(r));
+
+(async () => {
+  setImmediate(() => console.log(9));
+  console.log(10);
+  await sleep_st(0);
+  setImmediate(() => console.log(11));
+  console.log(12);
+  await sleep_im();
+  setImmediate(() => console.log(13));
+  console.log(14);
+  await 1;
+  setImmediate(() => console.log(15));
+  console.log(16);
+})();
+
+setImmediate(() => console.log(17));
+console.log(18);
+Promise.resolve().then(() => setTimeout(() => {
+  setImmediate(() => console.log(19));
+  console.log(20);
+  Promise.resolve().then(() => setImmediate(() => {
+    setImmediate(() => console.log(21));
+    console.log(22);
+    Promise.resolve().then(() => {
+      setImmediate(() => console.log(23));
+      console.log(24);
+    });
+  }));
+}, 0));
